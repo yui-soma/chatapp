27c13ab456f6045b86bb3f50ffce17ec20152dad
相馬由衣 <somayui@yuiMacBook-Pro.local>
2021-03-29 14:54:46 +0900

    materialUI追加

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,26 +1,32 @@
 import {Button, TextField, Typography} from '@material-ui/core';
 import {makeStyles} from '@material-ui/core/styles';
-import { findByLabelText } from '@testing-library/react';
 
 const useStyls = makeStyles({
     root: {
-        width: "350px",
+        width: "400px",
         height: "370px",
-        desplay: "flex",
-        flexDirection: "colum",
-        justifyContent: "space-bitween",
-    }
+        margin: "50px auto",
+        display: "flex",
+        flexDirection: 'column',
+        justifyContent: "space-between",
+    },
+    title: {
+        fontSize: "32px",
+        color:"red",
+    },
 });
 
 const Signup = () => {
     const classes = useStyls();
     return(
         <form className={classes.root}>
-            <Typography variant="h1">ユーザー登録ページ</Typography>
+            <Typography variant="h1" className={classes.title}>ユーザー登録ページ</Typography>
             <TextField variant="filled" label="メールアドレス" />
             <TextField variant="outlined" label="パスワード" />
             <TextField variant="standard" label="ユーザー名" />
-            <Button variant="contained">新規登録</Button>
+            <Button variant="contained" color="primary">
+                新規登録
+            </Button>
         </form>
     );
 };
