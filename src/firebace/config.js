b39da9ca7d8a3d388199d5b3ad98f7b1895c9d7d
相馬由衣 <somayui@yuiMacBook-Pro.local>
2021-04-02 06:45:01 +0900

    firebace挿入

---
+++ src/firebace/config.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyBP0p1VGPk-ihlCN8mybOPauGrvWhdP_tg",
+    authDomain: "chatapp-5da41.firebaseapp.com",
+    projectId: "chatapp-5da41",
+    storageBucket: "chatapp-5da41.appspot.com",
+    messagingSenderId: "522111073483",
+    appId: "1:522111073483:web:b4a5bb90e6c314b05daf4e",
+    measurementId: "G-XK48NYBGWR"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,35 @@
+import { Button, TextField, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyls = makeStyles({
+    root: {
+        width: "400px",
+        height: "370px",
+        margin: "50px auto",
+        display: "flex",
+        flexDirection: 'column',
+        justifyContent: "space-between",
+    },
+    title: {
+        fontSize: "32px",
+        color: "red",
+    },
+});
+
 const Login = () => {
+    const classes = useStyls();
     return (
-        <>
-            <h1>ログインページ</h1>
-        </>
+        <form className={classes.root}>
+            <Typography variant="h1" className={classes.title}>ログインページ</Typography>
+            <TextField variant="filled" label="メールアドレス" />
+            <TextField variant="outlined" label="パスワード" />
+            <Button variant="contained" color="secondary">
+                ログイン
+            </Button>
+        </form>
     );
 };
 
 export default Login;
+
+
